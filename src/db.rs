e9ca5fc146aee26aee192dd55a4a5e8762d26158
Charles Thompson <chmithbiz@gmail.com>
2023-02-18 15:46:12 -0500

    Changed license from bsd to gpl

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,21 @@
 //! Everything needed to interact with a pinv database
 
+// Copyright (c) 2023 Charles M. Thompson
+//
+// This file is part of pinv.
+//
+// pinv is free software: you can redistribute it and/or modify it under
+// the terms only of version 3 of the GNU General Public License as published
+// by the Free Software Foundation
+//
+// pinv is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// pinv(in a file named COPYING).
+// If not, see <https://www.gnu.org/licenses/>.
 use crate::b64;
 use chrono::{Local, TimeZone};
 use core::fmt;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+// Copyright (c) 2023 Charles M. Thompson
+//
+// This file is part of pinv.
+//
+// pinv is free software: you can redistribute it and/or modify it under
+// the terms only of version 3 of the GNU General Public License as published
+// by the Free Software Foundation
+//
+// pinv is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// pinv(in a file named COPYING).
+// If not, see <https://www.gnu.org/licenses/>.
 pub static ORGANISATION: &str = "Open Ape Shop";
 pub static APPLICATION: &str = "pinv";
 
